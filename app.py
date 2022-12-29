f3d797e3a195e2c886fd3944dfa4a5367dd12429
Luc Smith <93154131+LucS12@users.noreply.github.com>
2022-12-29 14:34:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ import allocation_model
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aaakkksllsjueujf'
 
+@app.route("/")
+def index():
+    return "go to https://dracar-portfolios.onrender.com/get-efficient-portfolios"
+
 #Route for the get url to retrieve the efficient portfolios:
 @app.route('/get-efficient-portfolios', methods=['GET'])
-def index():
+def portfolios():
     if request.method == 'GET':
         portfolios_json = allocation_model.efficient_allocations()
 
